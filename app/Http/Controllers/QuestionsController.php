0b262d8e523ce758be2902fb83c2172031482461
Arguss <roman.vyshedko@inbox.lv>
2018-11-13 23:44:54 +0200

    questions update form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -57,7 +57,7 @@ class QuestionsController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function edit(Question $question) {
-        return view ('questions.edit', compact($question));
+        return view("questions.edit", compact('question'));
     }
 
     /**
@@ -67,8 +67,9 @@ class QuestionsController extends Controller {
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question) {
-        //
+    public function update(AskQuestionRequest $request, Question $question) {
+        $question->update($request->only('title','body'));
+        return redirect()->route('questions.index')->with('success','Submited');
     }
 
     /**
--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
-
 @section('content')
+
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
                         <div class="d-flex align-items-center">
-                            <h2>Ask question</h2>
+                            <h2>Edit question</h2>
                             <div class="ml-auto">
                                 <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to
                                     questions</a>
@@ -16,8 +16,9 @@
                     </div>
 
                     <div class="card-body">
-                        <form action="{{route('questions.store')}}" method="POST">
-                            @include('questions._form',['buttonText'=>'Ask question'])
+                        <form action="{{route('questions.update',$question->id)}}" method="POST">
+                            {{method_field('PUT')}}
+                            @include('questions._form',['buttonText'=>'Update question'])
                         </form>
                     </div>
                 </div>
@@ -25,3 +26,7 @@
         </div>
     </div>
 @endsection
+
+
+
+
