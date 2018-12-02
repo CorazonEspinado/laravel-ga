14c7a3fe09df6de0e2a87a05b417c7d1af7923d5
Arguss <roman.vyshedko@inbox.lv>
2018-12-02 15:07:02 +0200

    votes color

---
+++ resources/views/answers/_create.blade.php
@@ -0,0 +1,20 @@
+
+
+<div class="row mt-4">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                   <h3>Your answer</h3>
+                </div>
+                <hr>
+               <form action="" method="POST">
+                   @csrf
+                   <div class="form-group">
+
+                   </div>
+               </form>
+            </div>
+        </div>
+    </div>
+</div>
