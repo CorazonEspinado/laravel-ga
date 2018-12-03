30223690de5a6e3805c4b9b61bc68c75b4f98992
Corazon Espinado <roman.vyshedko@inbox.lv>
2018-12-04 00:52:34 +0200

    update best answer id

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -14,7 +14,7 @@
                             <span class="votes-count">123</span>
                             <a title="I don`t Like it!" class="vote-down off"><i
                                     class="fas fa-caret-down fa-3x"></i></a>
-                            <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                            <a title="Mark this answer as best answer" class="{{$answer->status}} mt-2">
                                 <i class="fas fa-check fa-2x"></i>
 
                             </a>
