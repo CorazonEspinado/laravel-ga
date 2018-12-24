ba3cc5aad6e2d7e6885688f66f13235dfaa5313c
Arguss <roman.vyshedko@inbox.lv>
2018-12-25 01:38:28 +0200

    votes working

--- app/Answer.php
+++ app/Answer.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace App;
+
 use Illuminate\Database\Eloquent\Model;
+
 class Answer extends Model
 {
     protected $fillable = ['body', 'user_id'];
@@ -9,10 +12,12 @@ class Answer extends Model
     {
         return $this->belongsTo(Question::class);
     }
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
@@ -28,30 +33,37 @@ class Answer extends Model
             $answer->question->decrement('answers_count');
         });
     }
+
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
     }
+
     public function getStatusAttribute()
     {
         return $this->isBest() ? 'vote-accepted' : '';
     }
+
     public function getIsBestAttribute()
     {
         return $this->isBest();
     }
+
     public function isBest()
     {
         return $this->id === $this->question->best_answer_id;
     }
+
     public function votes()
     {
         return $this->morphToMany(User::class, 'votable');
     }
+
     public function upVotes()
     {
         return $this->votes()->wherePivot('vote', 1);
     }
+
     public function downVotes()
     {
         return $this->votes()->wherePivot('vote', -1);
