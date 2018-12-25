63fcd86cb525ae0d852084c0aaba3ffb34d5e336
Arguss <roman.vyshedko@inbox.lv>
2018-12-25 15:56:05 +0200

    new build

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use function foo\func;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 Use App\Question;
@@ -25,7 +26,10 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         Route::bind('slug', function ($slug) {
-            return Question::with('answers.user')->where('slug', $slug)->first() ?? abort(404);
+            return Question::with(['answers.user', 'answers'=>function($query) {
+                $query->orderBy('votes_count', 'DESC');
+                }])->where('slug', $slug)->first() ?? abort(404);
+//            return Question::with('answers.user')->where('slug', $slug)->first() ?? abort(404);
         });
 
         parent::boot();
